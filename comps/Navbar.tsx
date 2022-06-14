54277f8950de76903e5823814fa505f981296e9e
Rudy Yen <rudyyen@esdlife.com>
2022-06-14 16:40:10 +0800

    search bar init

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -36,7 +36,7 @@ const NavBar = () => {
     return (
       <nav className="grid sticky top-0 py-3.5 grid-cols-3 items-center justify-around bg-gradient-to-r from-zinc-100 via-slate-100 to-zinc-100 
       dark:text-slate-100 dark:bg-gradient-to-r dark:from-indigo-700 dark:via-purple-700 dark:to-fuchsia-800">
-        <div className="text-xl text-center font-family_header1">Auction House</div>
+        <Link href="/"><div className="text-xl text-center font-family_header1 cursor-pointer">Auction House</div></Link>
         <div className="flex text-base justify-center font-family_header1">
           <Link href="/">
             <div className={current_route.pathname == "/" ? "text-blue-500 dark:text-sky-300 mx-8 cursor-pointer" : "mx-8 cursor-pointer"}>Home</div>
