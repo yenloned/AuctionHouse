78ce350df57e73c692aac1689611e7d526e63810
unknown <yenloned@gmail.com>
2022-08-27 18:00:34 +0800

    local backend final

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
